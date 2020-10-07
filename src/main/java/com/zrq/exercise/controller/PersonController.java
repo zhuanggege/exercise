3e24860a488c13fe821f1526e7a86fa16b651307
zhuanggege <535034049@qq.com>
2020-10-07 17:11:16 +0800

    第一次修改提交10.7

--- src/main/java/com/zrq/exercise/controller/PersonController.java
+++ src/main/java/com/zrq/exercise/controller/PersonController.java
@@ -1,16 +1,18 @@
 package com.zrq.exercise.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PersonController {
 
     @RequestMapping("/index")
     @ResponseBody
     public String index () {
-
+        log.info("主页");
         return "hello world !! wellcome to springboot! ";
     }
 
