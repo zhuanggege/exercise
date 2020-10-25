c04fad2cbd9ca9f7874ceb14040e1f3eaab468b2
zhuanggege <535034049@qq.com>
2020-10-26 01:09:18 +0800

    develop

---
+++ src/main/java/com/zrq/exercise/mapper/UsersMapper.java
@@ -0,0 +1,16 @@
+package com.zrq.exercise.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zrq.exercise.entity.UsersModel;
+
+/**
+ * <p>
+ * 用户表	 Mapper 接口
+ * </p>
+ *
+ * @author zrq
+ * @since 2020-10-26
+ */
+public interface UsersMapper extends BaseMapper<UsersModel> {
+
+}
