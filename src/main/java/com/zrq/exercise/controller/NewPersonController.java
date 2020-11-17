6ca44bab1b179932ae767a9eba2d27dae2d812ac
zhuanggege <535034049@qq.com>
2020-11-17 22:48:34 +0800

    develop

--- src/main/java/com/zrq/exercise/controller/NewPersonController.java
+++ src/main/java/com/zrq/exercise/controller/NewPersonController.java
@@ -1,6 +1,9 @@
 package com.zrq.exercise.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/new")
+@Api(value = "新闻",tags = "新闻")
 public class NewPersonController {
 
-    @RequestMapping("/index")
+    @PostMapping("/index")
+    @ApiOperation(value = "主页",notes = "主页")
     public String index() {
 
         log.info("测试github分支和主支");
--- src/main/java/com/zrq/exercise/controller/PersonController.java
+++ src/main/java/com/zrq/exercise/controller/PersonController.java
@@ -2,18 +2,22 @@ package com.zrq.exercise.controller;
 
 import com.zrq.exercise.entity.Person;
 import com.zrq.exercise.server.PersonServer;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("person")
+@Api(value = "用户管理",tags = "用户管理")
 public class PersonController {
 
 //    @Autowired
@@ -23,7 +27,7 @@ public class PersonController {
     private PersonServer personServerImpl;
 
     @PostMapping({"/index","/"})
-    public String index () {
+    public String index (@Valid Integer id) {
         log.info("主页");
 //        QueryWrapper<Users> wrapper = new QueryWrapper<>();
 //        LambdaQueryWrapper<Users> eq = wrapper.lambda().eq(Users::getId, 2);
@@ -39,14 +43,15 @@ public class PersonController {
         return login;
 
     }
-    @RequestMapping("/findAll")
+    @PostMapping("/findAll")
+    @ApiOperation(value = "查询所有",notes = "查询所有")
     public List<Person> findAll() {
         List<Person> list = personServerImpl.findAll();
         log.info(list.toString());
         return list;
     }
 
-    @RequestMapping("/instPerson")
+    @PostMapping("/instPerson")
     public String insPerson(Person person) {
         Date date = new Date();
         long time = date.getTime();
